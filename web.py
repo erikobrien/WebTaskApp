915da06ec4b951f845134a776f678dba12e73e2f
erik <erikobrien@gmail.com>
2023-01-09 16:21:50 -0700

    added "add new item" functionality

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import functions
 
+def add_task():
+    new_task = st.session_state['new_task'] + '\n'
+    tasks.append(new_task)
+    functions.write_file(tasks)
+
+
 tasks = functions.open_file()
 
 st.title('My Tasks App')
@@ -8,4 +14,5 @@ st.title('My Tasks App')
 for task in tasks:
     st.checkbox(task)
 
-st.text_input(label='', placeholder='Add a new task...')
+st.text_input(label='', placeholder='Add a new task...',
+              on_change=add_task, key='new_task')
