6184637fe3e5b82c53b76af600e0a71d2392996d
erik <erikobrien@gmail.com>
2023-01-09 17:01:32 -0700

    add "complete" functionality. finished app

--- web.py
+++ web.py
@@ -5,14 +5,21 @@ def add_task():
     new_task = st.session_state['new_task'] + '\n'
     tasks.append(new_task)
     functions.write_file(tasks)
+    st.session_state['new_task'] = ' '
 
 
 tasks = functions.open_file()
 
 st.title('My Tasks App')
+st.write('Check the task when complete')
 
-for task in tasks:
-    st.checkbox(task)
+for index, task in enumerate(tasks):
+    checkbox = st.checkbox(task, key=task)
+    if checkbox:
+        tasks.pop(index)
+        functions.write_file(tasks)
+        del st.session_state[task]
+        st.experimental_rerun()
 
-st.text_input(label='', placeholder='Add a new task...',
+st.text_input(label=' ', placeholder='Add a new task...',
               on_change=add_task, key='new_task')
